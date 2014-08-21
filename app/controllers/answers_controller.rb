39be4f72c0756040a637e9a33e351c3a097ffe3c
Ben <benjaminwpeacock@gmail.com>
2014-08-21 19:25:38 +0000

    Save button on Answer#_edit and Answer#_new working

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,36 +5,37 @@ class AnswersController < ApplicationController
   # GET /answers.json
   def index
     @answers = Answer.all
-    authorize @answers
+    authorize @answer
   end
 
   # GET /answers/1
   # GET /answers/1.json
   def show
-    authorize @answers
+    authorize @answer
   end
 
   # GET /answers/new
   def new
     @answer = Answer.new
-    authorize @answers
+    authorize @answer
   end
 
   # GET /answers/1/edit
   def edit
-    authorize @answers
+    authorize @answer
   end
 
   # POST /answers
   # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
-    authorize @answers
+    authorize @answer
 
     respond_to do |format|
       if @answer.save
         format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @answer }
+        format.js
+        format.json { render json: @answer, status: :ok}
       else
         format.html { render action: 'new' }
         format.json { render json: @answer.errors, status: :unprocessable_entity }
@@ -45,11 +46,13 @@ class AnswersController < ApplicationController
   # PATCH/PUT /answers/1
   # PATCH/PUT /answers/1.json
   def update
-    authorize @answers
+    authorize @answer
     respond_to do |format|
       if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { head :no_content }
+        @answer.answer = @answer.answer.gsub(/\r\n?/, "<br/>").strip;
+        format.html { redirect_to [@program, @year], notice: 'Answer was successfully updated.' }
+        format.js { }
+        format.json { render json: @answer, status: :ok}
       else
         format.html { render action: 'edit' }
         format.json { render json: @answer.errors, status: :unprocessable_entity }
@@ -61,7 +64,7 @@ class AnswersController < ApplicationController
   # DELETE /answers/1.json
   def destroy
     @answer.destroy
-    authorize @answers
+    authorize @answer
     respond_to do |format|
       format.html { redirect_to answers_url }
       format.json { head :no_content }
