ab8d16e7c1cf52e17671f26753cabc9aa3cd4f46
Ben <benjaminwpeacock@gmail.com>
2014-08-25 19:16:20 +0000

    Pruning programs controller

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,5 @@
 class ProgramsController < ApplicationController
-  before_action :set_program, only: [:show, :edit, :update, :destroy]
+  before_action :set_program, only: [:show]
 
   # GET /programs
   # GET /programs.json
@@ -10,55 +10,6 @@ class ProgramsController < ApplicationController
   # GET /programs/1
   # GET /programs/1.json
   def show
-  end
-
-  # GET /programs/new
-  def new
-    @program = Program.new
-  end
-
-  # GET /programs/1/edit
-  def edit
-  end
-
-  # POST /programs
-  # POST /programs.json
-  def create
-    @program = Program.new(program_params)
-
-    respond_to do |format|
-      if @program.save
-        format.html { redirect_to @program, notice: 'Program was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @program }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @program.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /programs/1
-  # PATCH/PUT /programs/1.json
-  def update
-    respond_to do |format|
-      if @program.update(program_params)
-        format.html { redirect_to @program, notice: 'Program was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @program.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /programs/1
-  # DELETE /programs/1.json
-  def destroy
-    @program.destroy
-    respond_to do |format|
-      format.html { redirect_to programs_url }
-      format.json { head :no_content }
-    end
   end
 
   private
