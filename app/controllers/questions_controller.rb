701a59364fb60bf30ce5d13c386e796256577df9
Ben <benjaminwpeacock@gmail.com>
2014-08-25 19:04:07 +0000

    Removing unused views from programs and questions.  Removing unused methods from controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -53,13 +53,13 @@ class QuestionsController < ApplicationController
 
   # DELETE /questions/1
   # DELETE /questions/1.json
-  def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @question.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to questions_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
